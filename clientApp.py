f072627e6f98b6976ccbdbd0584793efa61720de
techner3 <58848985+techner3@users.noreply.github.com>
2022-04-01 14:41:39 +0530

    Logging Added

--- clientApp.py
+++ clientApp.py
@@ -3,6 +3,8 @@ import os
 from flask_cors import CORS, cross_origin
 from com_in_ineuron_ai_utils.utils import decodeImage, return_model
 from predictor import Model
+from logger import getLog
+
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
@@ -10,14 +12,21 @@ os.putenv('LC_ALL', 'en_US.UTF-8')
 app = Flask(__name__)
 CORS(app)
 
-
+logger=getLog('clientApp.py')
 
 
 #@cross_origin()
 class ClientApp:
     def __init__(self):
-        self.filename = "inputImage.jpg"
-        self.classifier = Model(self.filename)
+
+        try: 
+            self.filename = "inputImage.jpg"
+            self.classifier = Model(self.filename)
+            logger.info("ClientApp object initialized")
+
+        except Exception as e:
+            logger.exception(f"Failed to initialize App Object : \n{e}")
+            raise Exception("Failed to initialize App Object")
 
 
 
@@ -31,10 +40,15 @@ def home():
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
-    image = request.json['image']
-    decodeImage(image, clApp.filename)
-    result = clApp.classifier.prediction()
-    return jsonify(result)
+
+    try:
+        image = request.json['image']
+        decodeImage(image, clApp.filename)
+        result = clApp.classifier.prediction()
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify(e)
 
 
 #port = int(os.getenv("PORT"))
--- predictor.py
+++ predictor.py
@@ -1,21 +1,40 @@
 import numpy as np
 import tensorflow as tf
 from com_in_ineuron_ai_utils.utils import return_model
+from logger import getLog
+
+logger=getLog('predictor.py')
 
 class Model:
     def __init__(self,filename):
-        self.filename =filename
-        self.class_list=['Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal', 'Broccoli', 
+
+        try:
+            self.filename =filename
+            self.class_list=['Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal', 'Broccoli', 
         'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Cucumber', 'Papaya', 'Potato', 
         'Pumpkin', 'Radish', 'Tomato']
 
+        except Exception as e:
+            logger.exception(f"Failed to initialize Model Object : \n{e}")
+            raise Exception("Failed to initialize Model Object")
+
     def prediction(self):
-        model = return_model()
-        model.load_weights("TL_TF_VGG.h5")
-        imagename = self.filename
-        test_image = tf.keras.preprocessing.image.load_img(imagename, target_size = (224, 224))
-        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = model.predict(test_image)
-        prediction=self.class_list[np.argmax(result)]
-        return [{ "image" : prediction}]
+
+        try:
+            model = return_model()
+            logger.info("Model Created")
+            model.load_weights("TL_TF_VGG.h5")
+            logger.info("Weights loaded")
+            imagename = self.filename
+            test_image = tf.keras.preprocessing.image.load_img(imagename, target_size = (224, 224))
+            logger.info("Image loaded")
+            test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+            test_image = np.expand_dims(test_image, axis = 0)
+            result = model.predict(test_image)
+            logger.info("Prediction Completed")
+            prediction=self.class_list[np.argmax(result)]
+            return [{ "image" : prediction}]
+
+        except Exception as e:
+            logger.exception(f"Failed to make a prediction : \n{e}")
+            raise Exception("Failed to make a prediction")
